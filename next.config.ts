089f3718b435958dc8a29c737b32953c26560828
palmerbd <53416218+palmerbd@users.noreply.github.com>
2026-04-03 19:01:29 -0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/photo-*",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
